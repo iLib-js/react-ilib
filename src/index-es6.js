852d772b8ec5890a3131c07e69118fec24b7f7b7
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2019-07-14 01:10:24 -0700

    Initial clean-up and packaging for publication
    ... still needs more work though

---
+++ src/index-es6.js
@@ -0,0 +1,31 @@
+/*
+ * index.js - main js file for the react-ilib library
+ *
+ * Copyright © 2019, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export { AddressFmt } from './components/AddressFmt';
+export { ListFmt } from './components/ListFmt';
+export { NameFmt } from './components/NameFmt';
+export { NumFmt } from './components/NumFmt';
+export { PhoneFmt } from './components/PhoneFmt';
+export { UnitFmt } from './components/UnitFmt';
+
+export { LocaleContext } from './components/LocaleContext';
+export { LocaleDataProvider } from './components/LocaleDataProvider';
+export { Param } from './components/Param';
+export { Plural } from './components/Plural';
+export { Translate } from './components/Translate';
